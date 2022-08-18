abc3fbe06943d9c4a6005e2d2620fea858fe203d
Kevin Bell <bellKevin@pm.me>
2022-08-18 10:45:51 -0600

    Update BinarySearchDriver.java

--- recursiveBinarySearch-main/binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
+++ recursiveBinarySearch-main/binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
@@ -1,11 +1,34 @@
 //This drives the binarySearch class.
 package binarysearchdriver;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BinarySearchDriver {
 
     public static void main(String[] args) {
-        int[] array = new int[]{-7, 3, 5, 8, 12, 16, 23, 33, 55};
-        System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 23));
-        System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 4));
+        System.out.println("Sorting by Kevin Bell");
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        int arraySize, searchNumber;
+        System.out.print("How big is this array? ");
+        arraySize = computerKeyboardInput.nextInt();
+        int random = (int) (Math.random() * arraySize * 5 + 1);
+        System.out.println("Unsorted array: ");
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        for (int i=0; i< arraySize; i++) {
+            randomNumbers.add(random);
+            random = (int) (Math.random() * arraySize * 5 + 1);
+        }  
+        System.out.println("Array contains: "+randomNumbers);
+        System.out.println("Sorted array: ");
+        Collections.sort(randomNumbers);
+        System.out.println("Array contains: "+randomNumbers);
+        System.out.print("For which number should I search? (Type -1 to stop program) ");
+        searchNumber = computerKeyboardInput.nextInt();
+        // int[] array = new int[]{-7, 3, 5, 8, 12, 16, 23, 33, 55};
+        //System.out.print(array);
+        //System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 23));
+        //System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 4));
     }
 }
