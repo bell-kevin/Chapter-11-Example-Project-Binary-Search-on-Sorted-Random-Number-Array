e6853915d9f9550b07063ed58835d5f75d1c9648
Kevin Bell <bellKevin@pm.me>
2022-08-17 21:02:09 -0600

    Add files via upload

---
+++ recursiveBinarySearch-main/binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
@@ -0,0 +1,11 @@
+//This drives the binarySearch class.
+package binarysearchdriver;
+
+public class BinarySearchDriver {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-7, 3, 5, 8, 12, 16, 23, 33, 55};
+        System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 23));
+        System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 4));
+    }
+}
