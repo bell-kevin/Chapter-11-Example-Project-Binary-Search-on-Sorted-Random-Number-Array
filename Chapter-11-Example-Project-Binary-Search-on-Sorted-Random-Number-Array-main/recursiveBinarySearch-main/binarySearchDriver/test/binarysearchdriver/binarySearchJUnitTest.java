9f67b7ef74821676414ef99465a99881ea9d6b88
Kevin Bell <bellKevin@pm.me>
2022-08-19 13:59:52 -0600

    Update binarySearchJUnitTest.java

--- Chapter-11-Example-Project-Binary-Search-on-Sorted-Random-Number-Array-main/recursiveBinarySearch-main/binarySearchDriver/test/binarysearchdriver/binarySearchJUnitTest.java
+++ Chapter-11-Example-Project-Binary-Search-on-Sorted-Random-Number-Array-main/recursiveBinarySearch-main/binarySearchDriver/test/binarysearchdriver/binarySearchJUnitTest.java
@@ -13,4 +13,77 @@ public class binarySearchJUnitTest {
         int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
         assertEquals(expResult, result);
     }
+    
+    @Test
+    public void testBinary() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSearch() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBS() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBiSearch() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBinSearch() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBinaSearch() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBinarSearch() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBinarySear() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testBinarySea() {
+        System.out.println("binarySearch");
+        int[] randomNumbers = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
+        int expResult = 0;
+        int result = BinarySearch.binarySearch(randomNumbers, 0, 9, 1);
+        assertEquals(expResult, result);
+    }
 }
