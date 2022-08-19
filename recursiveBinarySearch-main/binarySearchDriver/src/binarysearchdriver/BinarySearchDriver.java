4782eaa843665803a37f1f84895fc1712c68f2fc
Kevin Bell <bellKevin@pm.me>
2022-08-18 19:17:34 -0600

    Update BinarySearchDriver.java

--- recursiveBinarySearch-main/binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
+++ recursiveBinarySearch-main/binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
@@ -8,34 +8,36 @@ public class BinarySearchDriver {
     public static void main(String[] args) {
         System.out.println("Sorting by Kevin Bell");
         Scanner computerKeyboardInput = new Scanner(System.in);
-        int arraySize, searchNumber;
+        int arraySize, searchNumber = 0;
         System.out.print("How big is this array? ");
         arraySize = computerKeyboardInput.nextInt();
         System.out.println("Unsorted array: ");
         int[] randomNumbers = new int[arraySize];
         System.out.print("Array contains: ");
-        for (int i=0; i<arraySize; i++){
+        for (int i = 0; i < arraySize; i++) {
             int randomNumber = (int) (Math.random() * arraySize * 5 + 1);
             randomNumbers[i] = randomNumber;
-            System.out.print(randomNumbers[i]+" ");
+            System.out.print(randomNumbers[i] + " ");
         } //end for loop 
         System.out.println();
         System.out.println("Sorted array: ");
         System.out.print("Array contains: ");
         Arrays.sort(randomNumbers);
-        for (int i=0; i<arraySize; i++){
-            System.out.print(randomNumbers[i]+" ");
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print(randomNumbers[i] + " ");
         } // end for loop
         System.out.println();
-        do {
-        System.out.print("For which number should I search? (Type -1 to end program) ");
-        searchNumber = computerKeyboardInput.nextInt();
-        if (searchNumber == -1){
-            System.out.println("Good bye");
-            break;
+        while (searchNumber != -1) {
+            System.out.print("For which number should I search? (Type -1 to end program) ");
+            searchNumber = computerKeyboardInput.nextInt();
+            int indexPosition = BinarySearch.binarySearch(randomNumbers, 0, (randomNumbers.length - 1), searchNumber);
+            if (indexPosition != -1) {
+                System.out.println("it was found in position " + indexPosition);
+            }
+            if (indexPosition == -1) {
+                System.out.println("that number was not found");
+            }
         }
-        //binarysearchdriver.BinarySearch.binarySearch(randomNumbers, 0, (arraySize-1), searchNumber);
-        System.out.println(BinarySearch.binarySearch(randomNumbers, 0, (randomNumbers.length - 1), searchNumber));
-        } /* end do */ while (searchNumber != -1);
+        System.out.println("Good bye");
     } // end main
 } // end BinarySearchDriver class
