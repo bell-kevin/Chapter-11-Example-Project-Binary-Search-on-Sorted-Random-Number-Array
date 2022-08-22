21942ef9d0ca8fdca93effd965675db642c96dd3
Kevin Bell <bellKevin@pm.me>
2022-08-22 18:57:17 +0000

    Update Ch11exProjBinarySearch.java

--- ch11exProjBinarySearch/src/ch11exprojbinarysearch/Ch11exProjBinarySearch.java
+++ ch11exProjBinarySearch/src/ch11exprojbinarysearch/Ch11exProjBinarySearch.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch11exprojbinarysearch;
+
 import java.util.*;
-/**
- *
- * @author 4800590195
- */
+
 public class Ch11exProjBinarySearch {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Sorting by Kevin Bell");
         Scanner computerKeyboardInput = new Scanner(System.in);
         int arraySize, searchNumber = 0;
